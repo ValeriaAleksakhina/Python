bc8adf9391a7d5ea2b1ddf2fdb78734b8be29854
Valeria_Aleksakhina <Virago400@mail.ru>
2023-02-21 12:48:14 +0400

    added seminar2

--- Homework/1/Task8.py
+++ Homework/1/Task8.py
@@ -12,9 +12,11 @@ n = int (input("Введите число n: "))
 m = int (input("Введите число m: "))
 k = int (input("Введите число k: "))
 
-if k % n == 0:
+if (k%n ==0 or k%m==0) and k<n*m:
+
+#if k % n == 0:
     print ("Yes")
-elif k % m == 0:
-    print ("Yes")
+#elif k % m == 0:
+    #print ("Yes")
 else:
     print ("No")
