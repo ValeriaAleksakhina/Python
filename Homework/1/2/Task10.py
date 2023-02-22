73489cabc0497f455a4ac4244eb3b406ca4b76dc
Valeria_Aleksakhina <Virago400@mail.ru>
2023-02-22 16:34:33 +0400

    added task10

--- Homework/1/2/Task10.py
+++ Homework/1/2/Task10.py
@@ -3,4 +3,20 @@
 чтобы все монетки были повернуты вверх одной и той же стороной.
 Выведите минимальное количество монет, которые нужно перевернуть"""
 
-n = int (input ())
+import random
+n = int(input("Введите количество монеток: "))
+count_orel = 0
+count_reshka = 0
+for i in range(n):
+    x = random.randint(0, 1)
+    print (f"сторонa {i+1} = {x}")
+    if x == 0:
+        count_orel += 1
+    else:
+        count_reshka += 1
+if count_reshka > count_orel:
+    print(count_orel)
+else:
+    print(count_reshka)
+
+
